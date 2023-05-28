3330d6540da9656b4a895ae36801d575cb683878
betancourtfit <beta.juan.c@gmail.com>
2023-05-28 01:17:00 -0300

    agregue validacion de DNI en la base

--- entrega-final/index.js
+++ entrega-final/index.js
@@ -2,112 +2,118 @@
 // Aun no he podido hacer que funcionen las importaciones
 //import {formatDate, convertirFecha} from './modulos/fechas.js';
 
+
+
+/* 
+---------------------------------------------------------
+Esta es la función que me permite importar los datos desde google sheets al inicio de la sesion 
+---------------------------------------------------------
+*/
 // Declaracion de variables y constantes
 const sheetID = '1sDzQURSh6jqT4GAOyW-spRsGXUiPFgexOgyYhh0en0E';
 const base = `https://docs.google.com/spreadsheets/d/${sheetID}/gviz/tq?`;
 const sheetName = 'BBDD_Memb';
-let dni = 31446785;
+let dni = 95489618;
 let qu = `Select A,B,C,D,G WHERE A = ${dni}`
 let query = encodeURIComponent(qu);
 let data = [];
 let url = `${base}&sheet=${sheetName}&tq=${query}`;
 const output = document.querySelector('.output');
 
-
-/* 
----------------------------------------------------------
-Este es el event listener que esta a la espera del envio de datos para iniciar la funcion de validacion del dni Store
----------------------------------------------------------
-*/
-
-document.addEventListener("DOMContentLoaded", function() {
-    const submitButton = document.getElementById("submitButton");
-    submitButton.addEventListener("click",executeInit);
-});
-
-/* 
----------------------------------------------------------
-Este es el event listener que esta a la espera del envio de datos para iniciar la funcion de validacion del dni Store
----------------------------------------------------------
-*/
-
-async function executeInit() {
-    // Mostrar el modal de espera
-    console.log("inicio modal 1")
-    const waitingModal = new bootstrap.Modal(document.getElementById("waitingModal"), {
-        backdrop: "static",
-        keyboard: false
-    });
-    waitingModal.show();
-    console.log("inicio modal 2")
-
-    // Ejecutar la función init y esperar a que se complete
-    await validarDni();
-    console.log("inicio modal 3")
-
-    // Ocultar el modal de espera
-    waitingModal.hide();
-}
-
-/* 
----------------------------------------------------------
-Esta es la funcion que valida que el número de DNI sea válido y de ser asi ejecuta la funcion init
----------------------------------------------------------
-*/
-function validarDni() {
-    dni = parseInt(document.getElementById("dniInput").value);
-    if (dni > 1000) {
-        // Deja un log de que el dni fue validado
-        console.log("DNI válido:");
-        
-        init();
-    } else {
-        // Muestra un mensaje de error
-        alert("DNI inválido, por favor reingrese");
-    }
-}
-
-/* 
----------------------------------------------------------
-Esta es la funcion principal que trae y prepara la informacion desde el archivo de google sheets
----------------------------------------------------------
-*/
-function init(){
+// Etsa es la funcion que hae el fetch de la data y la procesa para convertirla en un objeto javascript
+function init() {
     // Hago la consulta con el fetch que dentro tiene un filtro en sql del numero de DNI y  despues la respuesta la transformo en un objeto json
     data = [];
     qu = `Select A,B,C,D,G WHERE A = ${dni}`;
     query = encodeURIComponent(qu);
     url = `${base}&sheet=${sheetName}&tq=${query}`;
     console.log('ready');
+    
     fetch(url)
-    .then(res => res.text())
-    .then(rep => {
-        const jsData = JSON.parse(rep.substring(47).slice(0,-2));
-        console.log(jsData);
-        const colz = [];
-        //Extraigo del json en un array los rotulos de las columnas
-        jsData.table.cols.forEach((heading)=>{
-            console.log(heading);
-            if(heading.label){
-                colz.push(heading.label.toLowerCase().replace(/\s/g,''));
+        .then(res => res.text())
+        .then(rep => {
+            const jsData = JSON.parse(rep.substring(47).slice(0,-2));
+            console.log(jsData);
+            const colz = [];
+            // Extraigo del json en un array los rotulos de las columnas
+            jsData.table.cols.forEach((heading)=>{
+                console.log(heading);
+                if (heading.label) {
+                    colz.push(heading.label.toLowerCase().replace(/\s/g,''));
+                }
+            });
+            // Hago una iteración para poder construir una matriz a partir de las celdas de cada fila
+            jsData.table.rows.forEach((main)=>{
+                console.log(main);
+                const row = {};
+                colz.forEach((ele,ind)=>{
+                    console.log(ele);
+                    row[ele] = (main.c[ind] != null) ? main.c[ind].v : '';  
+                });
+                data.push(row);
+            });
+            
+            for (let i = 0; i < data.length; i++) {
+                  // Obtén los valores de año, mes y día del atributo 'INGRESO' en cada objeto
+                const matches = data[i].ingreso.match(/\d+/g);
+                console.log(matches)
+                
+                const year = Number(matches[0]);
+                const month = Number(matches[1]);
+                console.log(year,month)
+                    
+                const day = Number(matches[2]);
+                // Crea un objeto DateTime con Luxon utilizando los valores de año, mes y día
+                const fecha = luxon.DateTime.fromObject({
+                        year: year,
+                        month: month
+                });
+                
+                // Formatea la fecha utilizando Luxon
+                let f = {month: 'long', year: 'numeric'}
+                const formatoFecha = fecha.setLocale('es-ES').toLocaleString(f);
+                // Asigna el valor formateado al atributo 'INGRESO' en cada objeto
+                data[i].ingreso = formatoFecha;
+                
             }
+        // console.log(convertirFecha(data[0].ingreso.value));
+        console.log(typeof(data));       
+        sessionStorage.setItem("dataAlmacenada", JSON.stringify(data));    
+        console.log(typeof(dataAlmacenada));
         })
-        //Hago una iteración para poder construir una matriz a partir de las celdas de cada fila
-        jsData.table.rows.forEach((main)=>{
-            console.log(main);
-            const row = {};
-            colz.forEach((ele,ind)=>{
-                console.log(ele);
-                row[ele] = (main.c[ind] != null) ? main.c[ind].v : '';  
-            })
-            data.push(row)
-        })
-        //console.log(convertirFecha(data[0].ingreso.value));
-
-        maker(data);
-    })
-
+        .catch(error => {
+            // Mostrar mensaje de error con SweetAlert
+            swal("Error", error.message, "error");
+        });
 }
+
+//Ejecuto la funcion init para poder traer la data de google sheets
+init()
+
+//Mensaje inicial en el que solicito el DNI para poderlo constrastar con la base de datos
+
+const { value: dniNuevo } = await Swal.fire({
+    title: 'Este sitio es exclusivo para socios',
+    input: 'number',
+    inputLabel: 'Ingrese su DNI en números',
+    inputValue: dni,
+    showCancelButton: true,
+    inputValidator: (value) => {
+        let a = JSON.parse(sessionStorage.getItem("dataAlmacenada"))[0].dni
+    if (value != a) {
+        return 'Este DNI no pertenece un socio'
+    }
+    }
+})
+
+if (dniNuevo) {
+    sessionStorage.setItem("dni",dniNuevo)
+    Swal.fire(`Su DNI es ${ sessionStorage.getItem("dni")}`)
+    let dataAlmacenada = sessionStorage.getItem("dataAlmacenada");
+    let data = JSON.parse(dataAlmacenada);
+    maker(data)
+}
+
 
 /* 
 ---------------------------------------------------------
@@ -145,3 +151,60 @@ function maker(json){
     })
 })
 }
+
+
+/* 
+---------------------------------------------------------
+Este es el event listener que esta a la espera del envio de datos para iniciar la funcion de validacion del dni Store
+---------------------------------------------------------
+*/
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     const submitButton = document.getElementById("submitButton");
+//     submitButton.addEventListener("click",executeInit);
+// });
+
+/* 
+---------------------------------------------------------
+Este es el event listener que esta a la espera del envio de datos para iniciar la funcion de validacion del dni Store
+---------------------------------------------------------
+*/
+
+// async function executeInit() {
+//     // Mostrar el modal de espera
+//     console.log("inicio modal 1")
+//     const waitingModal = new bootstrap.Modal(document.getElementById("waitingModal"), {
+//         backdrop: "static",
+//         keyboard: false
+//     });
+//     waitingModal.show();
+//     console.log("inicio modal 2")
+
+//     // Ejecutar la función init y esperar a que se complete
+//     await validarDni();
+//     console.log("inicio modal 3")
+
+//     // Ocultar el modal de espera
+//     waitingModal.hide();
+// }
+
+/* 
+---------------------------------------------------------
+Esta es la funcion que valida que el número de DNI sea válido y de ser asi ejecuta la funcion init
+---------------------------------------------------------
+*/
+// function validarDni() {
+//     dni = parseInt(document.getElementById("dniInput").value);
+//     if (dni > 1000) {
+//         // Deja un log de que el dni fue validado
+//         console.log("DNI válido:");
+        
+//         init();
+//     } else {
+//         // Muestra un mensaje de error
+//         alert("DNI inválido, por favor reingrese");
+//     }
+// }
+
+
+
